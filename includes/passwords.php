49358cb56a1510de5d2c5a88de83f1a243264ba8
Mia Nordentoft <mia@miestasmia.com>
2017-07-10 15:20:30 +0200

    Added passwords.php

---
+++ includes/passwords.php
@@ -0,0 +1,28 @@
+<?php
+    /*
+     * Copyright (C) 2017 Mia Nordentoft, Anton S. Meiner, GitHub contributors
+     *
+     * This file is part of dejo.dk.
+     *
+     * dejo.dk is free software: you can redistribute it and/or modify
+     * it under the terms of the GNU General Public License as published by
+     * the Free Software Foundation, either version 3 of the License, or
+     * (at your option) any later version.
+     *
+     * dejo.dk is distributed in the hope that it will be useful,
+     * but WITHOUT ANY WARRANTY; without even the implied warranty of
+     * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+     * GNU General Public License for more details.
+     *
+     * You should have received a copy of the GNU General Public License
+     * along with dejo.dk. If not, see <http://www.gnu.org/licenses/>.
+     */
+    
+    define('SMTP', [
+          'host' => '',
+          'port' => '',
+          'auth' => true,
+          'username' => '',
+          'password' => '',
+          'secure' => 'ssl'
+     ]);
