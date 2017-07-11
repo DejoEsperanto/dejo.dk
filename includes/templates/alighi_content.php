e9f7a9e6ca01edfe6a1d740e227271caaf588cb5
Mia Nordentoft <mia@miestasmia.com>
2017-07-11 20:24:13 +0200

    Responsive registration

--- includes/templates/alighi_content.php
+++ includes/templates/alighi_content.php
@@ -33,6 +33,12 @@
                          <input name="firstname" id="form_firstname" tabindex="1" maxlength="80" required>
                     </div>
                     <div>
+                         <label for="form_lastname"><?=LSTR['pages']['alighi']['form']['lastname']?></label>
+                         <input name="lastname" id="form_lastname" tabindex="2" maxlength="80">
+                    </div>
+               </div>
+               <div>
+                    <div>
                          <label for="form_birthday"><?=LSTR['pages']['alighi']['form']['birthday']?>*</label>
                          <?php
                               $max = new DateTime();
@@ -41,36 +47,12 @@
                          ?>
                          <input name="birthday" id="form_birthday" tabindex="3" type="date" max="<?=$max->format('Y-m-d')?>" min="<?=$min->format('Y') + 1?>-01-01" required>
                     </div>
+               </div>
+               <div>
                     <div>
                          <label for="form_address"><?=LSTR['pages']['alighi']['form']['address']?></label>
                          <input name="address" id="form_address" tabindex="4" maxlength="200">
                     </div>
-                    <div>
-                         <label for="form_zipcode"><?=LSTR['pages']['alighi']['form']['zipcode']?></label>
-                         <input id="form_zipcode" tabindex="6" name="zipcode" maxlength="20">
-                    </div>
-                    <div>
-                         <label for="form_email"><?=LSTR['pages']['alighi']['form']['email']?>*</label>
-                         <input name="email" id="form_email" tabindex="8" type="email" maxlength="200" required>
-                    </div>
-                    <div>
-                         <label for="form_phone"><?=LSTR['pages']['alighi']['form']['phone']?></label>
-                         <input name="phone" id="form_phone" tabindex="9" type="tel" maxlength="20">
-                    </div>
-                    <div class="checkbox">
-                         <input name="individual" id="form_individual" tabindex="10" type="checkbox">
-                         <label for="form_individual"><?=LSTR['pages']['alighi']['form']['individual']?></label>
-                    </div>
-                    <div>
-                         <div class="g-recaptcha" data-sitekey="6LczlSgUAAAAAHdaXdWPN2aFrWDAmaneGHEaGb2v"></div>
-                    </div>
-               </div>
-               <div>
-                    <div>
-                         <label for="form_lastname"><?=LSTR['pages']['alighi']['form']['lastname']?></label>
-                         <input name="lastname" id="form_lastname" tabindex="2" maxlength="80">
-                    </div>
-                    <div></div>
                     <div>
                          <label for="form_country"><?=LSTR['pages']['alighi']['form']['country']?>*</label>
                          <select name="country" id="form_country" tabindex="5" maxlength="30" required>
@@ -86,15 +68,38 @@
                               ?>
                          </select>
                     </div>
+               </div>
+               <div>
+                    <div>
+                         <label for="form_zipcode"><?=LSTR['pages']['alighi']['form']['zipcode']?></label>
+                         <input id="form_zipcode" tabindex="6" name="zipcode" maxlength="20">
+                    </div>
                     <div>
                          <label form="form_city"><?=LSTR['pages']['alighi']['form']['city']?></label>
                          <input id="form_city" name="city" tabindex="7" maxlength="50">
                     </div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
+               </div>
+               <div>
                     <div>
-                         <br/>
+                         <label for="form_email"><?=LSTR['pages']['alighi']['form']['email']?>*</label>
+                         <input name="email" id="form_email" tabindex="8" type="email" maxlength="200" required>
+                    </div>
+               </div>
+               <div>
+                    <div>
+                         <label for="form_phone"><?=LSTR['pages']['alighi']['form']['phone']?></label>
+                         <input name="phone" id="form_phone" tabindex="9" type="tel" maxlength="20">
+                    </div>
+               </div>
+               <div>
+                    <div class="checkbox">
+                         <input name="individual" id="form_individual" tabindex="10" type="checkbox">
+                         <label for="form_individual"><?=LSTR['pages']['alighi']['form']['individual']?></label>
+                    </div>
+               </div>
+               <div id="final">
+                    <div class="g-recaptcha" data-sitekey="6LczlSgUAAAAAHdaXdWPN2aFrWDAmaneGHEaGb2v"></div>
+                    <div>
                          <input type="submit" tabindex="10" value="<?=LSTR['pages']['alighi']['form']['submit']?>">
                     </div>
                </div>
