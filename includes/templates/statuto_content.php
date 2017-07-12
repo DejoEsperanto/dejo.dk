6667ceb6c9b4c48faa3c4140ab0374ef9c54c961
Mia Nordentoft <mia@miestasmia.com>
2017-07-12 15:30:15 +0200

    Statuto

---
+++ includes/templates/statuto_content.php
@@ -0,0 +1,112 @@
+<?php
+    /*
+     * Copyright (C) 2017 Mia Nordentoft, Anton S. Meiner, GitHub contributors
+     *
+     * This file is part of dejo.dk.
+     *
+     * dejo.dk is free software: you can redistribute it and/or modify
+     * it under the terms of the GNU General Public License as published by
+     * the Free Software Foundation, either version 3 of the License, or
+     * (at your option) any later version.
+     *
+     * dejo.dk is distributed in the hope that it will be useful,
+     * but WITHOUT ANY WARRANTY; without even the implied warranty of
+     * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+     * GNU General Public License for more details.
+     *
+     * You should have received a copy of the GNU General Public License
+     * along with dejo.dk. If not, see <http://www.gnu.org/licenses/>.
+     */
+?>
+  
+<p class="p"><?=LSTR['nurdana']?></p>
+
+<ol class="legal">
+     <li>
+          <b>Navn &amp; tilhørsforhold</b>
+          <p>Foreningens navn er Dana Esperantista Junulara Organizo (DEJO). Foreningen er stiftet i 1986. DEJO er selvstændig sektion af DEA og landsektion af TEJO (jf. TEJOs love).</p>
+     </li>
+     <li>
+          <b>Formål</b>
+          <p>DEJOs formål er at fremme og styrke anvendelsen af esperanto blandt ungdommen i Danmark.</p>
+     </li>
+     <li>
+          <b>Medlemskab</b>
+          <ol class="ol">
+               <li>DEJO består af aktive medlemmer, interesserede og æresmedlemmer.</li>
+               <li>Aktive medlemmer er alle medlemmer i DEJO, som er yngre end 30 år, eller medlemmer, som har en funktion i DEJO og er yngre end 35 år.</li>
+               <li>Interesserede er personer eller organisationer, som modtager nyt fra foreningen, og kan deltage i aktiviteterne i en periode til de bliver medlemmer.</li>
+               <li>Æresmedlemmer er personer, som specielt har udmærket sig i foreningens arbejde.</li>
+               <li>Indmeldelse sker til Esperantoforeningen for Danmarks (DEA) kasserer.</li>
+          </ol>
+     </li>
+     <li>
+          <b>Udmeldelse</b>
+          <ol class="ol">
+               <li>Lokalforeninger eller enkelte medlemmer, som åbenbart modarbejder foreningens mål, kan udelukkes af foreningen efter foreningsledelsens beslutning. Udelukket forening eller et udelukket medlem har ret til at klage over beslutningen til årsmødet.</li>
+               <li>Medlemskab ophører ved udgangen af det kalenderår hvori medlemmet fylder 30 år. Funktionærer i DEJO kan vælges til de er 35 år.</li>
+               <li>Medlem eller forening, som trods påmindelse ikke har betalt afgiften i løbet af det forgange kalenderår, anses som værende udtrådt af foreningen.</li>
+               <li>Ved udelukkelse af medlem af DEA eller TEJO udelukkes medlemmet automatisk af DEJO.</li>
+          </ol>
+     </li>
+     <li>
+          <b>Kontingent</b>
+          <p>Medlemmer betaler kontingent til DEAs kasserer.</p>
+     </li>
+     <li>
+          <b>Meddelelser</b>
+          <ol class="ol">
+               <li>Meddelelser fra bestyrelsen til foreninger og øvrige medlemmer skal ske på den mest formålstjenlige måde.</li>
+               <li>Alle betalende medlemmer modtager DEAs officielle organ “Esperanto en Danio”.</li>
+          </ol>
+     </li>
+     <li>
+          <b>Bestyrelsen</b>
+          <ol class="ol">
+               <li>DEJO ledes af en bestyrelse, der består af forperson og mindst 3 andre medlemmer, og som konstituerer sig med næstformand, kasserer og sekretær.</li>
+               <li>Er forpersonen forhindret, træder næstformanden i vedkommendes sted indtil førstkommende generalforsamling.</li>
+               <li>Ved valg af bestyrelsen vælges forpersonen for et år og tre øvrige bestyrelsesmedlemmer for to år.</li>
+               <li>Bestyrelsen er beslutningsdygtig, når mere end halvdelen af bestyrelsesmedlemmer deltager. I tilfælde af stemmelighed er forpersonens stemme afgørende.</li>
+               <li>Bestyrelsen står til ansvar for sine handlinger på årsmødet.</li>
+          </ol>
+     </li>
+     <li>
+          <b>Årsmøde</b>
+          <ol class="ol">
+               <li>DEJOs årsmøde skal holdes i løbet af kalenderårets første seks måneder. Det skal indkaldes mindst en måned forud.</li>
+               <li>Om foreningens virksomhed i det forgangne år udarbejdes en beretning, som samtidig med forslag til dagsordnen oplæses på årsmødet. Forslag til optagelse på dagsorden skal fremsættes skriftligt overfor forpersonen senest 3 uger før årsmødet og meddeles medlemmerne senest en uge i forvejen.</li>
+               <li>Stemmeret har kun de medlemmer, der har betalt forfalden kontingent. Ved årsmødet har medlemmer som er nærværende, bortset fra understøttende medlemmer, hver en stemme. Ikke nærværende medlemmer, bortset fra understøttende, kan stemme ved andet medlem, som skal fremvise fuldmagt.</li>
+               <li>Alle beslutninger, bortset fra dem, som behandles i § 13 og § 14 kræver en enkelt stemmeovertægt. Ved lige stemmetal foretages lodtrækning.</li>
+               <li>Skriftlig afstemning foretages, hvis et medlem ønsker det.</li>
+               <li>
+                    Dagsorden skal indeholde mindst følgende punkter:
+                    <ol class="ol">
+                         <li>Valg af ordstyrer og referent for årsmødet</li>
+                         <li>Bestyrelsens beretning</li>
+                         <li>Årsresultat</li>
+                         <li>Forslag</li>
+                         <li>Valg af forperson</li>
+                         <li>Valg af bestyrelsesmedlemmer</li>
+                         <li>Valg af repræsentanter i TEJO og DEA, efter behov</li>
+                    </ol>
+               </li>
+          </ol>
+     </li>
+     <li>
+          <b>Ekstraordinært årsmøde</b>
+          <p>Bestyrelsen kan, med mindst 4 ugers varsel og med angivelse af dagsorden, indkalde til ekstraordinært årsmøde, hvis mindst ⅓ af medlemmerne har afgivet skriftlig begæring om dette. Forslag kan afgives af ethvert medlem. De bør være bestyrelsen i hænde mindst to uger før årsmådet.</p>
+     </li>
+     <li>
+          <b>Ændringer af love</b>
+          <p>Forslag til ændringer, tillæg eller ophævelse af bestemmelser i DEJOs love skal sendes til alle medlemmer mindst en måned før ordinært årsmøde. Forslaget skal være godkendt med ⅔ majoritet af årsmødet, for at forslaget skal anses godkendt.</p>
+     </li>
+     <li>
+          <b>Opløsning af DEJO</b>
+          <ol class="">
+               <li>Beslutning om opløsning af DEJO kan kun træffes af et årsmøde efter samme procedure som ved ændringer af love.</li>
+               <li>Ved opløsning af foreningen skal dets eventuelle midler anvendes til et formål, som kan anses at være på linje med foreningens formål.</li>
+          </ol>
+     </li>
+</ol>
+
+<p>Vedtægter med ændringer vedtaget den 25. april 2004.</p>
