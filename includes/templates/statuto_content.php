8112b45e34cc411b7f08c244ea2a60304b9ffd2b
Mia Nordentoft <mia@miestasmia.com>
2023-12-26 22:36:59 +0100

    Ĝisdatigis la statuton

--- includes/templates/statuto_content.php
+++ includes/templates/statuto_content.php
@@ -24,11 +24,40 @@
 <ol class="legal">
      <li>
           <b>Navn &amp; tilhørsforhold</b>
-          <p>Foreningens navn er “Dana Esperantista Junulara Organizo” (DEJO) på esperanto og “Dansk Esperanto Ungdomsforening” på dansk. Foreningen er stiftet i 1986. DEJO er selvstændig sektion af Dana Esperanto-Asocio (DEA) og landsektion af Tutmonda Esperantista Junulara Organizo (TEJO)jf. TEJOs love.</p>
+          <p>Foreningens navn er “Dana Esperantista Junulara Organizo” (DEJO) på esperanto og “Dansk Esperanto Ungdomsforening” på dansk. Foreningen er stiftet i 1986. DEJO er selvstændig sektion af Dana Esperanto-Asocio (DEA) og landsektion af Tutmonda Esperantista Junulara Organizo (TEJO) jf. TEJOs vedtægter. DEJO er registreret i CVR-registreret med CVR-nummer 42509310 som en almindelig forening.</p>
      </li>
      <li>
           <b>Formål</b>
-          <p>DEJOs formål er at fremme og styrke anvendelsen af esperanto blandt ungdommen i Danmark.</p>
+          <ol class="ol">
+               <li>DEJOs arbejde er hovedsageligt med børn og unge under 35 år og deres personlige,
+kulturelle, sociale, politiske og samfundsmæssige udvikling med fokus på internationale
+problemstillinger, specifikt indenfor området international kommunikation og det
+sprogmæssige. DEJO arbejder på at uddanne unge mennesker til at blive ansvarlige borgere
+i samfundet, som respekterer andre mennesker og deres værdi samt præstationer, på at
+skabe tætte forbindelser mellem danske unge og unge i andre lande indenfor alle kulturelle
+områder og internationalt samarbejde vedr. de problemstillinger, som rører unge
+mennesker.</li>
+               <li>Hjørnestenen af DEJOs arbejde er i nationale såvel som internationale ungdoms-
+begivenheder, -seminarier, og -kurser, hvorigennem DEJO promoverer international
+korrespondance og engagement med målet på neutral vis at skabe en direkte kontakt med
+andre folkeslag, deres kulturer samt skik og brug, såvidt muligt på et personligt niveau for
+den enkelte unge. DEJO arbejder også på at opnå dette mål igennem internationale
+projekter med henblik på udveksling af unge med dets partnerorganisationer indenrigs
+såvel som udenrigs.</li>
+               <li>DEJO anvender det internationale sprog esperanto til sit arbejde og arbejder for at styrke
+dets anvendelse blandt ungdommen først og fremmest i Danmark, men også internationalt.
+DEJO er overbevist om, at anvendelsen af det neutrale sprog esperanto udgør et vigtigt,
+réelt og ligefremt bidrag til at skabe et tillidsfuldt, fredeligt og betydningsfuldt samarbejde
+blandt forskellige kulturer.</li>
+          </ol>
+     </li>
+     <li>
+          <b>Status som almennyttig forening</b>
+          <p>DEJO er en almennyttig forening, som arbejder for almenets nytte. Foreningen arbejder aldrig med
+formålet at skabe profit for sine medlemmer eller bestyrelsesmedlemmer. DEJO arbejder altid
+inden for rammerne af dansk lovgivning og EU-ret.
+Et eventuelt overskud fra DEJOs aktiviteter skal altid anvendes til formål, der går under det
+foregående afsnit om foreningens formål.</p>
      </li>
      <li>
           <b>Medlemskab</b>
@@ -37,13 +66,13 @@
                <li>Aktive medlemmer er alle medlemmer i DEJO, som er yngre end 35 år i begyndelsen af året.</li>
                <li>Interesserede er personer eller organisationer, som modtager nyt fra foreningen, og kan deltage i aktiviteterne i en periode til de bliver medlemmer.</li>
                <li>Æresmedlemmer er personer, som specielt har udmærket sig i foreningens arbejde.</li>
-               <li>Indmeldelse sker enten gennem DEJO eller DEA til DEAs kasserer.</li>
+               <li>Indmeldelse sker til DEJOs kasserer.</li>
           </ol>
      </li>
      <li>
           <b>Udmeldelse</b>
           <ol class="ol">
-               <li>Lokalforeninger eller enkelte medlemmer, som tydeligt modarbejder foreningens mål, kan udelukkes af foreningen efter bestyrelsens beslutning. Udelukket forening eller udelukket medlem har ret til at klage over beslutningen til årsmødet.</li>
+               <li>Lokalforeninger eller enkelte medlemmer, som tydeligt modarbejder foreningens mål, kan udelukkes af foreningen efter bestyrelsens beslutning. Udelukket forening eller udelukket medlem har ret til at klage over beslutningen til generalforsamlingen.</li>
                <li>Medlemskab ophører ved udgangen af det kalenderår hvori medlemmet fylder 35 år.</li>
                <li>Medlem eller forening, som trods påmindelse ikke har betalt afgiften i løbet af det forgange kalenderår, anses som værende udtrådt af foreningen.</li>
                <li>Ved udelukkelse af medlem af DEA eller TEJO udelukkes medlemmet automatisk af DEJO.</li>
@@ -51,37 +80,48 @@
      </li>
      <li>
           <b>Kontingent</b>
-          <p>Medlemmer betaler kontingent til DEAs kasserer.</p>
+          <p>Medlemmer betaler kontingent til DEJOs kasserer. Kontingentet fastsættes af medlemmerne ved
+en generalforsamling.</p>
+     </li>
+     <li>
+          <b>Regnskab og sprog</b>
+          <p>DEJOs regnskabsår er det almindelige kalenderår. DEJO anvender både dansk og esperanto til
+internt arbejde.</p>
      </li>
      <li>
           <b>Meddelelser</b>
           <ol class="ol">
                <li>Meddelelser fra bestyrelsen til foreninger og øvrige medlemmer skal ske på den mest formålstjenlige måde.</li>
-               <li>Alle betalende medlemmer modtager DEAs officielle organ “Esperanto en Danio” med posten.</li>
+               <li>Alle betalende medlemmer modtager DEAs officielle organ “Esperanto en Danio” (EeD)
+med posten. Fra 2024 sendes EeD udelukkende per e-mail. Medlemmer, der ønsker at
+modtage EeD med posten må fra 2024 betale et ekstragebyr fastsat af medlemmerne ved en
+generalforsamling.</li>
           </ol>
      </li>
      <li>
           <b>Bestyrelsen</b>
           <ol class="ol">
-               <li>DEJO ledes af en bestyrelse, der består af forperson og mindst 2 andre medlemmer: næstforperson og sekretær.</li>
+               <li>DEJO ledes af en bestyrelse, der består af forperson og mindst 2 andre medlemmer: næstforperson og kasserer.</li>
                <li>Er forpersonen forhindret, træder næstforpersonen i vedkommendes sted indtil førstkommende generalforsamling.</li>
                <li>Ved valg af bestyrelsen vælges forpersonen for et år og mindst to øvrige bestyrelsesmedlemmer for to år.</li>
                <li>Bestyrelsen er beslutningsdygtig når mere end halvdelen af bestyrelsesmedlemmer deltager. I tilfælde af stemmelighed er forpersonens stemme afgørende.</li>
-               <li>Bestyrelsen står til ansvar for sine handlinger på årsmødet.</li>
+               <li>Bestyrelsen står til ansvar for sine handlinger på generalforsamlingen.</li>
           </ol>
      </li>
      <li>
-          <b>Årsmøde</b>
+          <b>Generalforsamling</b>
           <ol class="ol">
-               <li>DEJOs årsmøde skal holdes i løbet af kalenderårets første seks måneder. Det skal indkaldes mindst en måned forud.</li>
-               <li>Om foreningens virksomhed i det forgangne år udarbejdes en beretning, som samtidig med forslag til dagsordnen oplæses på årsmødet. Forslag til optagelse på dagsorden skal fremsættes skriftligt overfor forpersonen senest 3 uger før årsmødet og meddeles medlemmerne senest en uge i forvejen.</li>
-               <li>Stemmeret har kun de medlemmer, der har betalt forfalden kontingent. Ved årsmødet har medlemmer som er nærværende hver én stemme. Ikke nærværende medlemmer kan stemme ved andet medlem, som skal fremvise fuldmagt.</li>
-               <li>Alle beslutninger, bortset fra dem, som behandles i § 10 og § 11 kræver en enkelt stemmeovertægt. Ved lige stemmetal er forpersonens stemme afgørende.</li>
+               <li>DEJOs generalforsamling skal holdes i løbet af kalenderårets første seks måneder. Det skal indkaldes mindst en måned forud.</li>
+               <li>Om foreningens virksomhed i det forgangne år udarbejdes en beretning, som samtidig med forslag til dagsordnen oplæses på generalforsamlingen. Forslag til optagelse på dagsorden skal fremsættes skriftligt overfor forpersonen senest 3 uger før generalforsamlingen og meddeles medlemmerne senest en uge i forvejen.</li>
+               <li>Kassereren eller et andet bestyrelsesmedlem skal ved generalforsamlingen præsentere en
+årsopgørelse over foreningens finanser for det forrige finansielle år.</li>
+               <li>Stemmeret har kun de medlemmer, der har betalt forfalden kontingent. Ved generalforsamlingen har medlemmer som er nærværende hver én stemme. Ikke nærværende medlemmer kan stemme ved andet medlem, som skal fremvise fuldmagt.</li>
+               <li>Alle beslutninger, bortset fra dem, som behandles i § 11 og § 12 kræver en enkelt stemmeovertægt. Ved lige stemmetal er forpersonens stemme afgørende.</li>
                <li>Skriftlig afstemning foretages, hvis et medlem ønsker det.</li>
                <li>
                     Dagsorden skal indeholde mindst følgende punkter:
                     <ol class="ol">
-                         <li>Valg af ordstyrer og referent for årsmødet</li>
+                         <li>Valg af ordstyrer og referent for generalforsamlingen</li>
                          <li>Referat af det forgangne år</li>
                          <li>Forslag</li>
                          <li>Valg af forperson</li>
@@ -93,17 +133,17 @@
           </ol>
      </li>
      <li>
-          <b>Ekstraordinært årsmøde</b>
-          <p>Bestyrelsen kan med mindst 4 ugers varsel og med angivelse af dagsorden indkalde til ekstraordinært årsmøde. Forslag kan afgives af ethvert betalende medlem senest to uger før årsmødet.</p>
+          <b>Ekstraordinær generalforsamling</b>
+          <p>Bestyrelsen kan med mindst 4 ugers varsel og med angivelse af dagsorden indkalde til ekstraordinær generalforsamling. Forslag kan afgives af ethvert betalende medlem senest to uger før generalforsamlingen.</p>
      </li>
      <li>
           <b>Ændringer af love</b>
-          <p>Forslag til ændringer, tillæg eller ophævelse af bestemmelser i DEJOs love skal sendes til alle medlemmer mindst en måned før ordinært årsmøde, eller to uger inden ekstraordinært årsmøde. Forslaget skal være godkendt med ⅔ majoritet af årsmødet for at forslaget anses godkendt.</p>
+          <p>Forslag til ændringer, tillæg eller ophævelse af bestemmelser i DEJOs love skal sendes til alle medlemmer mindst en måned før ordinær generalforsamling, eller to uger inden ekstraordinær generalforsamling. Forslaget skal være godkendt med ⅔ majoritet af generalforsamlingen for at forslaget anses godkendt.</p>
      </li>
      <li>
           <b>Opløsning af DEJO</b>
-          <p>Beslutning om opløsning af DEJO kan kun træffes ved et årsmøde efter samme procedure som ved ændringer af love. Ved opløsning af foreningen skal dets eventuelle midler anvendes til et formål som kan anses at være på linje med foreningens mål og så vidt muligt videregives til DEA.</p>
+          <p>Beslutning om opløsning af DEJO kan kun træffes ved en generalforsamling efter samme procedure som ved ændringer af love. Ved opløsning af foreningen skal dets eventuelle midler anvendes til et formål som kan anses at være på linje med foreningens mål og så vidt muligt videregives til DEA.</p>
      </li>
 </ol>
 
-<p>Vedtægter med ændringer vedtaget den 8. august 2021.</p>
+<p>Vedtægter med ændringer vedtaget den 25. marts 2023.</p>
